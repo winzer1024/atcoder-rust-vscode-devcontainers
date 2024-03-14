b032db0aa6c1be60f4362d707032e9b1f4e8fab1
sugimountain <1220055@ed.tus.ac.jp>
2024-03-15 01:16:14 +0900

    create compete.toml

---
+++ abc338/src/bin/a.rs
@@ -0,0 +1,24 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+#[fastout]
+fn main() {
+    input!{
+        s: Chars,
+    }
+    let temp = s.iter().rposition(|&c| c.is_uppercase()).unwrap_or(1);
+    println!("{}",if temp == 0 {"Yes"} else {"No"});
+}
